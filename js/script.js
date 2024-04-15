83e38555d72f57d076b6a9b2dacad31a33b04e89
Abhinav1326 <abhinavrbgore13@gmail.com>
2024-04-15 16:35:19 +0530

    Responsive

--- js/script.js
+++ js/script.js
@@ -430,9 +430,15 @@ function scaleImage(img, ctx) {
   var hRatio = canvas.width / img.width;
   var vRatio = canvas.height / img.height;
   var ratio = Math.max(hRatio, vRatio);
+
+
   var centerShift_x = (canvas.width - img.width * ratio) / 2;
   var centerShift_y = (canvas.height - img.height * ratio) / 2;
+
+  
+
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
   ctx.drawImage(
     img,
     0,
@@ -445,6 +451,7 @@ function scaleImage(img, ctx) {
     img.height * ratio
   );
 }
+
 ScrollTrigger.create({
   trigger: "#page>canvas",
   pin: true,
